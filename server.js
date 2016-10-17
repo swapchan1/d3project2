a23b4a131bde07095ce7575bd9fa158e1f524eb3
Aziz Khilawala <khilawalaaziz@gmail.com>
2016-10-17 08:58:57 -0400

    server.js change

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('/Users/azizkhilawala/Documents/Repositories/d3project2'));
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+app.listen(3002, function() {
+    console.log('Example app listening on port 3002!');
 });
